7ca8f096d082c3f2f7ea30ffe84bd40ea0d150b6
job moore <jobmurdan@ohtmail.com>
2024-03-23 10:31:37 -0700

    Cree el script OperadoresExpresiones.lua que me sirve para entender las partes mas complejas de las operaciones, comparadores, and or y not, asi como la concatenacion en Lua

---
+++ OperadoresExpresiones.lua
@@ -0,0 +1,29 @@
+        local resultado = 10 + 5   --igual a 15
+        local resta = 10 - 5   --igual a 5
+        local multiplicacion = 10 * 5   --igual a 50
+        local division = 10 / 5   --igual a 2
+--OPERACIONES COMPLEJAS
+local modulo = 10 % 3   --igual a 1
+local potencia = 10 ^ 2   --igual a 100
+
+    local a = 10
+    local b = 5
+--COMPARADORES
+local igualdad = a == b   --falso
+local diferencia = a ~= b   --verdadero
+local menorQue = a < b   --falso
+local mayorQue = a > b   --verdadero
+
+    local x = true
+    local y = false
+
+--AND OR Y NOT
+local resultadoY = x and y   --falso
+local resultadoO = x or y   --verdadero
+local resultadoNo = not x   --falso
+
+    local nombre = "Juan"
+    local apellido = "Pérez"
+--CONCATENACION DE STRINGS
+local nombreCompleto = nombre .. " " .. apellido   
+
